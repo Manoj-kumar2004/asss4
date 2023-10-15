766c13a38312d9bd3970b3c3a7b0cd6424827007
Manoj <manojkumar2k4.g@gmail.com>
2023-10-15 21:12:21 +0530

    new file:   SpeakGoodBye.js 	new file:   SpeakHello.js 	new file:   index.html 	new file:   script.js commit

---
+++ SpeakGoodBye.js
@@ -0,0 +1,8 @@
+
+(
+function byeSpeaker(name) {
+  var speakWord = "Good Bye";
+  console.log(speakWord + " " + name);
+window.byeSpeaker=byeSpeaker;
+})(window)
+
---
+++ SpeakHello.js
@@ -0,0 +1,8 @@
+
+
+(
+function helloSpeaker(name) {
+  var speakWord = "Hello";
+  console.log(speakWord + " " + name);
+window.helloSpeaker=helloSpeaker;
+})(window)
